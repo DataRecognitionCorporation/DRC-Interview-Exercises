f01c90d8f6ba779e27b28dc9fcf7511f74d2f06b
Dan Bellandi <dan.bellandi@gmail.com>
2021-08-24 15:30:15 -0500

    remove solution

--- app.ts
+++ app.ts
@@ -1,18 +1,7 @@
-import fetch from 'node-fetch';
 
-const URL = 'https://gorest.co.in/public/v1/users';
-
-const PATTERN = /gmail.com/;
-
-async function getUsers() {
-  const result = await fetch(URL);
-  return await result.json();
-}
 
 (async () => {
-  let data = await getUsers();
 
-  let emails = data.data.filter(r => r.email.match(PATTERN)).map(r => r.email);
+  // entry point
 
-  for (let x of emails) console.log(x);
 })();
