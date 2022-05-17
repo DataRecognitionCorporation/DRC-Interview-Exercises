87f4ca58330fc57fddb97d9f0cfc86e4b4122f5c
Marty Robertson <marobertson@drcadmins-MacBook-Pro.local>
2022-05-17 14:58:51 -0500

    add stubs for get questions exercise

--- app.ts
+++ app.ts
@@ -1,18 +1,29 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import { json, urlencoded } from 'body-parser';
+const sourceQuestions = require('./mock_data/questions');
 
 const app = express();
 const port = 3000;
 
 // Parse json request body middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.post('/', (req, res) => {
   // Access request body json: req.body
   res.send("Hello! - Post request received. Typescript version running.");
 });
 
+app.get('/questions', (req, res) => {
+  // Access QS params via: req.query
+  // Mock question JSON accessed via variable: sourceQuestions
+  res.send({
+    message: "Hello! - GET request received. Typescript version running."
+  });
+});
+
 app.listen(port, () => {
   return console.log(`server is listening on http://localhost:${port}`);
 });
+
+// RESTART APP ON CODE CHANGES AFTER SAVING FILE(s) !!
